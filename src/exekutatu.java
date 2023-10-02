9706c9913fd99cd725b0e43c06c294cd28dd46e5
Mikel <maretxaga@tolosaldealh.eus>
2023-10-02 12:59:36 +0200

    Lehenengo commita

---
+++ src/exekutatu.java
@@ -0,0 +1,12 @@
+public class exekutatu {
+	public static void main(String[] args) {
+		System.out.println("Kaixo mundua");
+        int i = 0;
+        while(i>=0) {
+        	System.out.println(i);
+        	i++;
+        }
+        
+        }
+
+}
