ccf16eed678e9037f03596c3c17dc9b0e9c0cc8a
FeedbackTolosaldea <FeedbackTolosaldea@gmail.com>
2023-10-02 13:00:54 +0200

    Handiago berdin jarri beharrean handiago jarri dut

--- src/exekutatu.java
+++ src/exekutatu.java
@@ -2,7 +2,7 @@ public class exekutatu {
 	public static void main(String[] args) {
 		System.out.println("Kaixo mundua");
         int i = 0;
-        while(i>=0) {
+        while(i>0) {
         	System.out.println(i);
         	i++;
         }
